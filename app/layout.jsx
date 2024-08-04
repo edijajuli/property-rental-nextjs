b46b8df6cd6bfdce76a81e6f9c6d6bd768476ab5
edijajuli <edijajuli1@gmail.com>
2024-08-05 02:21:12 +0700

    styling homepage component & footer component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import "@/assets/styles/globals.css";
 
 export const metadata = {
@@ -12,6 +13,7 @@ const Mainlayout = ({ children }) => {
       <body>
         <Navbar />
         <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
