1c0fdc7e68a29e13d30a404cf7ccfb045fedf7fe
edijajuli <edijajuli1@gmail.com>
2024-09-07 00:00:33 +0700

    displaying profile image

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    ignoreDuringBuilds: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
