50612d7bf5c0a83546301a6a46192df5c83b11e8
edijajuli <edijajuli1@gmail.com>
2024-09-06 22:58:16 +0700

    Add Next auth and provider set up

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import "@/assets/styles/globals.css";
 import AuthProvider from "@/components/AuthProvider";
+import "@/assets/styles/globals.css";
 
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
